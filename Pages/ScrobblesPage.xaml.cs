4a472eb1ae1ab13b8237be038bbfb96b912ef53e
James Madden <jamesbmadden@gmail.com>
2023-02-28 16:42:52 -0500

    Load recent scrobbles from the last.fm API

--- Pages/ScrobblesPage.xaml.cs
+++ Pages/ScrobblesPage.xaml.cs
@@ -9,28 +9,46 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using WinFM.Types;
 
 namespace WinFM.Pages {
-  /// <summary>
-  /// An empty page that can be used on its own or navigated to within a Frame.
-  /// </summary>
+
   public sealed partial class ScrobblesPage : Page {
+
+    // data that can be used for rendering the recent scrobbles
+    RecentScrobblesResponse recentScrobbles;
+
     public ScrobblesPage() {
       this.InitializeComponent();
 
       // load the user's recent scrobbles from the lastfm api
+      LoadRecentScrobbles();
+
     }
 
-    void SwitchPage (object sender, RoutedEventArgs e) {
-      // hide the loader and show the content
-      LoadingView.Visibility = Visibility.Collapsed;
-      ScrobblesView.Visibility = Visibility.Visible;
+    // get the user's recent scrobbles from the last fm api
+    async void LoadRecentScrobbles() {
+      HttpClient client = new HttpClient();
+
+      client.DefaultRequestHeaders.Add("User-Agent", "WinFM");
+
+      string responseJson = await client.GetStringAsync("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jamesbmadden&format=json&api_key=" + Api.Key);
+
+      recentScrobbles = JsonSerializer.Deserialize<RecentScrobblesResponse>(responseJson);
+
+      // now response is a proper c# object containing all the data from the API!
+      // set the list view to read from the recent scrobbles
+      RecentScrobblesList.ItemsSource = recentScrobbles.recenttracks.track;
+
     }
   }
 }
---
+++ Types/ApiResponses.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WinFM.Types {
+
+  public class RecentScrobblesResponse {
+    public Recenttracks recenttracks { get; set; }
+  }
+
+  public class Recenttracks {
+    public Track[] track { get; set; }
+    [JsonPropertyName("@attr")]
+    public Attr attr { get; set; }
+  }
+
+  public class Attr {
+    public string user { get; set; }
+    public string totalPages { get; set; }
+    public string page { get; set; }
+    public string perPage { get; set; }
+    public string total { get; set; }
+  }
+
+  public class Track {
+    public Artist artist { get; set; }
+    public string streamable { get; set; }
+    public Image[] image { get; set; }
+    public string mbid { get; set; }
+    public Album album { get; set; }
+    public string name { get; set; }
+    public string url { get; set; }
+    public Date date { get; set; }
+  }
+
+  public class Artist {
+    public string mbid { get; set; }
+    [JsonPropertyName("#text")]
+    public string text { get; set; }
+  }
+
+  public class Album {
+    public string mbid { get; set; }
+    [JsonPropertyName("#text")]
+    public string text { get; set; }
+  }
+
+  public class Date {
+    public string uts { get; set; }
+    [JsonPropertyName("#text")]
+    public string text { get; set; }
+  }
+
+  public class Image {
+    public string size { get; set; }
+    [JsonPropertyName("#text")]
+    public string text { get; set; }
+  }
+
+}
